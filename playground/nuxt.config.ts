42cd95c594a6dd0136ffa1cd12b66537d07394cb
Manuel Odelain <4610549+manuelodelain@users.noreply.github.com>
2026-03-31 18:04:28 +0200

    feat: move stories playground to its own nuxt app

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -2,19 +2,8 @@ import { fileURLToPath } from 'url'
 import { dirname, join } from 'path'
 
 const currentDir = dirname(fileURLToPath(import.meta.url))
-const nuxtStoriesEnabled = process.env.NUXT_STORIES === '1'
 
 export default defineNuxtConfig({
-    modules: [
-        ...(nuxtStoriesEnabled ? ['../src/module'] : []),
-    ],
-    stories: {
-        enabled: nuxtStoriesEnabled,
-        mode: 'shell',
-        frameCwd: '.',
-        framePort: 3000,
-        publicAssetsDir: 'stories/assets',
-    },
     css: [
         join(currentDir, './app/assets/css/main.css')
     ],
---
+++ playground/stories/nuxt.config.ts
@@ -0,0 +1,11 @@
+export default defineNuxtConfig({
+    extends: '../',
+    modules: [
+        '../../src/module'
+    ],
+    stories: {
+        mode: 'shell',
+        frameCwd: '../',
+        framePort: 3000,
+    },
+})
